b5b6957bb0f2f9b1761a7fcbae0a7e7baff142ab
muhammadessam <muhammad159e@gmail.com>
2025-10-02 06:41:24 +0300

    Google Maps Styles

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../generated/assets.dart';
 
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
@@ -21,12 +24,26 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     31.263277207445874,
   );
   late LatLng _currentLocation;
+  late String _currentStyle;
 
   @override
   void initState() {
     super.initState();
     _initialCameraPosition = CameraPosition(target: _homeLocation, zoom: 19);
     _currentLocation = _homeLocation;
+    _currentStyle = Assets.nightMapStyle;
+  }
+
+  Future<void> initMapStyle() async {
+    final styleJson = await rootBundle.loadString(Assets.nightMapStyle);
+    _googleMapController.setMapStyle(styleJson);
+    _currentStyle = Assets.nightMapStyle;
+  }
+
+  changeMapStyle(String style) async {
+    final styleJson = await rootBundle.loadString(style);
+    _googleMapController.setMapStyle(styleJson);
+    _currentStyle = style;
   }
 
   @override
@@ -43,31 +60,55 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
           initialCameraPosition: _initialCameraPosition,
           onMapCreated: (controller) {
             _googleMapController = controller;
+            initMapStyle();
           },
         ),
         Positioned(
           bottom: 20,
           left: 20,
-          child: FloatingActionButton.large(
-            onPressed: () {
-              if (_currentLocation == _homeLocation) {
-                _currentLocation = _workLocation;
-              } else {
-                _currentLocation = _homeLocation;
-              }
-              _googleMapController.animateCamera(
-                CameraUpdate.newLatLng(_currentLocation),
-              );
-              setState(() {});
-            },
-            child: Text(
-              'Go to ${_currentLocation == _homeLocation
-                  ? "Work"
-                  : _workLocation == _currentLocation
-                  ? "Home"
-                  : "Unknown"}',
-            ),
-            // icon: const Icon(Icons.add),
+          child: Row(
+            spacing: 20,
+            children: [
+              FloatingActionButton.extended(
+                onPressed: () {
+                  if (_currentLocation == _homeLocation) {
+                    _currentLocation = _workLocation;
+                  } else {
+                    _currentLocation = _homeLocation;
+                  }
+                  _googleMapController.animateCamera(
+                    CameraUpdate.newLatLng(_currentLocation),
+                  );
+                  setState(() {});
+                },
+                label: Text(
+                  'Go to ${_currentLocation == _homeLocation
+                      ? "Work"
+                      : _workLocation == _currentLocation
+                      ? "Home"
+                      : "Unknown"}',
+                ),
+                // icon: const Icon(Icons.add),
+              ),
+              FloatingActionButton.extended(
+                onPressed: () {
+                  changeMapStyle(
+                    _currentStyle == Assets.nightMapStyle
+                        ? Assets.retroMapStyle
+                        : Assets.nightMapStyle,
+                  );
+                  setState(() {});
+                },
+                label: Text(
+                  'Change Map Style to ${_currentStyle == Assets.nightMapStyle
+                      ? "Silver"
+                      : _currentStyle == Assets.retroMapStyle
+                      ? "Night"
+                      : "Unknown"}',
+                ),
+                // icon: const Icon(Icons.add),
+              ),
+            ],
           ),
         ),
       ],
